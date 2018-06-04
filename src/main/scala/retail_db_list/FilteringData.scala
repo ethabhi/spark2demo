1a3f6bc867cfcfc0dc40c10fc5cc6a6659248569
ethabhi <abhishek.polsani@yahoo.com>
2018-06-04 07:58:14 -0400

    args included

--- src/main/scala/retail_db_list/FilteringData.scala
+++ src/main/scala/retail_db_list/FilteringData.scala
@@ -3,7 +3,6 @@ package retail_db_list
 import SparkSql.SparkSql.sparkSession
 import utils.sparkCont
 
-
 object FilteringData extends App with sparkCont {
 
   val orders = sparkSession.read
@@ -11,7 +10,7 @@ object FilteringData extends App with sparkCont {
     .option("inferSchema", "false")
     .csv("C:\\data\\retail_db\\orders")
     .toDF("orderId", "date", "customerId", "status")
-
+ 
   val ordersFil =orders.filter( e => e(3) == "COMPLETE")
 
   ordersFil.take(10).foreach(println)
